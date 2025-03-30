d7d7eb0b46e6fb6831e559eb22bf7432011dea81
ConquestSucks <91684467+ConquestSucks@users.noreply.github.com>
2025-03-30 21:46:11 +0500

    added chat storage service

--- src/utils/storage.ts
+++
@@ -1,9 +0,0 @@
-export function getChatMessages(room: string) {
-    const storedMessages = localStorage.getItem(`chat-${room}`);
-    return storedMessages ? JSON.parse(storedMessages) : [];
-  }
-  
-  export function saveChatMessages(room: string, messages: unknown[]) {
-    localStorage.setItem(`chat-${room}`, JSON.stringify(messages));
-  }
-  
